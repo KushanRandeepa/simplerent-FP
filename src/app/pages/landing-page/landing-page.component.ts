fadeffb2a9deb6d0a4c7a0ede68c4d0151628c96
KushanRandeepa <kushanrandeepa55@gmail.com>
2025-08-01 18:09:53 +0530

    landing page v1

---
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -0,0 +1,111 @@
+import { NgFor } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { initFlowbite } from 'flowbite';
+
+@Component({
+  selector: 'app-landing-page',
+  imports: [RouterLink,NgFor],
+  templateUrl: './landing-page.component.html',
+  styleUrl: './landing-page.component.css'
+})
+export class LandingPageComponent implements OnInit {
+
+  ngOnInit(): void {
+       initFlowbite();
+
+  }
+
+  scrollTo(sectionId: string) {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' })
+    }
+  }
+
+  benifitsList:string[]=[
+    ' Local Platform',
+    'Easy Booking Process',
+    'Reliable Local Support',
+    'Residential & Commercial Rentals',
+    'Secure Listings',
+    ' Simple, Transparent Process'
+  ]
+  guestList:string[]=[
+      ' Explore a variety of listings to suit your budget and lifestyle.',
+      'Reserve your stay securely with instant confirmation.',
+      'Move in and relax—your space is ready when you are.',
+      'Flexible cancellation',
+      'Book instantly, and enjoy 24/7 support'
+    ]
+  hoststList:string[]=[
+    'Earn More',
+    'Receive secure payments after each confirmed booking.',
+    'List Your Space',
+    'Decide your rates and availability',
+    ' Book instantly, and enjoy 24/7 support'
+  ]
+  
+  serviceCard: Card[] = [
+  {
+    title: 'Fast Booking',
+    description: 'Book your stay in seconds with our optimized system.',
+    icon: '/images/pexels-pixabay-276514.jpg' 
+  },
+  {
+    title: 'second card',
+    description: 'Get the best deals for a comfortable experience.',
+    icon: '/images/pexels-pixabay-276514.jpg'
+  },
+  {
+    title: 'Affordable Rates',
+    description: ',Get the best deals for a comfortable experience.',
+    icon: '/images/pexels-pixabay-276514.jpg'
+  },
+  
+  {
+    title: 'Affordable Rates',
+    description: 'Get the best deals for a comfortable experience.',
+    icon: '/images/pexels-pixabay-276514.jpg'
+  }
+];
+
+faqList:faq[]=[
+  {question:'Is Simple Rent free to use?',
+    answer:'A: Browsing is free. Listing and booking may include service fees (shown during checkout).'
+  },
+  {question:'Do you verify listings?',
+    answer:'A: Yes, we manually verify listings and offer support to ensure authenticity and safety.'
+  },
+  {question:'How do I list my property?',
+    answer:'A: Just sign up, upload photos, enter details, and go live in minutes.'
+  },
+  {question:'Who can use Simple Rent?',
+    answer:'A: Anyone looking to rent or list residential or commercial properties in Sri Lanka.'
+  },
+  {question:'What types of rentals are available?',
+    answer:'A: Homes, apartments, workspaces, shops, venues, and more'
+  }
+]
+
+
+
+
+  login() {
+
+  }
+
+}
+
+
+
+interface Card {
+  title: string;
+  description: string;
+  icon: string; 
+}
+interface faq{
+  question:string,
+  answer:string
+}
+
